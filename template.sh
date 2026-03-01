e76b83cd9ad37bc0fb05f97ff06be5767373e256
Kotha Ashwin <kothaashwin@Kothas-MacBook-Air.local>
2026-03-01 17:53:53 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating Directory
+mkdir -p src # -p - hypen p (parameter)
+mkdir -p research
+
+# Creating Files
+touch src/___init__.py # Constructor file created inside src folder
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb # Experiment File
+touch requirement.txt 
+
+
+echo "Directory and Files created successfully!." # Once everything is created it will give a terminal message.
